c8cf9076b3b3c9d5c20e3fbce8b36876c54ebe3e
MarinchenkoValeriia <valery_2003_valery@ukr.net>
2024-03-13 23:22:40 +0200

    lab_2 is done

--- Lab_2/lab_2.py
+++ Lab_2/lab_2.py
@@ -5,41 +5,34 @@ import openpyxl
 wb = openpyxl.load_workbook('data.xlsx')
 ws = wb.active
 
-x_train_0 = [ws.cell(row=i,column=1).value for i in range(1,ws.max_row+1)]
-x_train_1 = [ws.cell(row=i,column=2).value for i in range(1,ws.max_row+1)]
-x_train = []
-x_train_item = []
-for i in range(len(x_train_0)):
-    x_train_item.append(x_train_0[i])
-    x_train_item.append(x_train_1[i])
-    x_train.append(x_train_item)
-    x_train_item = []
+# Читаємо дані з файлу Excel та конвертуємо їх у числовий формат
+x_train_0 = np.array([float(ws.cell(row=i,column=1).value) for i in range(2, ws.max_row+1)])
+x_train_1 = np.array([float(ws.cell(row=i,column=2).value) for i in range(2, ws.max_row+1)])
+y_train = np.array([float(ws.cell(row=i,column=3).value) for i in range(2, ws.max_row+1)])
 
-y_train = [ws.cell(row=i,column=3).value for i in range(1,ws.max_row+1)]
+# Об'єднуємо дані у вхідні вектори
+x_train = np.column_stack((x_train_0, x_train_1, np.ones_like(x_train_0)))
 
-#x_train = [[10, 50], [20, 30], [25, 30], [20, 60], [15, 70], [40, 40], [30, 45], [20, 45], [40, 30], [7, 35]]
-x_train = [x + [1] for x in x_train]  # Додаємо зміщення для кожного прикладу
-x_train = np.array(x_train)
-y_train = np.array(y_train)
+# Обчислення вагових коефіцієнтів
+w = np.linalg.lstsq(x_train, y_train, rcond=None)[0]
+print("Weights:", w)
 
-pt = np.sum([x * y for x, y in zip(x_train, y_train)], axis=0)  # Обчислення підсумку
-xxt = np.sum([np.outer(x, x) for x in x_train], axis=0)  # Обчислення підсумку зовнішнього добутку
-w = np.dot(pt, np.linalg.inv(xxt))  # Обчислення вагових коефіцієнтів
-print(w)
+# Формуємо координати для лінії розділення
+line_x = np.arange(0, np.max(x_train[:, 0]) + 1)
+line_y = -(w[0] * line_x + w[2]) / w[1]
 
-line_x = list(range(max(x_train[:, 0])))    # формування координат для лінії розділення
-line_y = [-x*w[0]/w[1] - w[2]/w[1] for x in line_x]
+# Розділяємо дані на класи
+x_0 = x_train[y_train == 1]
+x_1 = x_train[y_train == -1]
 
-x_0 = x_train[y_train == 1]                 # формування точок для класу 1
-x_1 = x_train[y_train == -1]                # і класу -1
-
-plt.scatter(x_0[:, 0], x_0[:, 1], color='red')  # відображення точок класу 1 червоним кольором
-plt.scatter(x_1[:, 0], x_1[:, 1], color='blue')  # відображення точок класу -1 синім кольором
-plt.plot(line_x, line_y, color='green')  # відображення лінії розділення зеленим кольором
-
+# Відображаємо графік
+plt.scatter(x_0[:, 0], x_0[:, 1], color='red')
+plt.scatter(x_1[:, 0], x_1[:, 1], color='blue')
+plt.plot(line_x, line_y, color='green')
 plt.xlim([0, 60])
 plt.ylim([0, 51])
 plt.ylabel("довжина")
 plt.xlabel("ширина")
 plt.grid(True)
+plt.legend()
 plt.show()

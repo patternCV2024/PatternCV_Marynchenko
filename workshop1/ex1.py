114fbafbf5e401c9ac949e47f6be05a7c68f73db
MarinchenkoValeriia <valery_2003_valery@ukr.net>
2024-04-11 04:26:39 +0300

    workshop1 total

--- workshop1/ex1.py
+++ workshop1/ex1.py
@@ -1,84 +1,35 @@
-
-import os
-import numpy as np
 import tensorflow as tf
-#from tensorflow import keras
-
-# Load the data
-
-# Get current working directory
-current_dir = os.getcwd()
-
-# Append data/mnist.npz to the previous path to get the full path
-data_path = os.path.join(current_dir, "data/mnist.npz")
-
-# Get only training set
-(training_images, training_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+import numpy as np
 
 
-# GRADED FUNCTION: reshape_and_normalize
+# GRADED FUNCTION: house_model
+def house_model():
+    ### START CODE HERE ###
 
-def reshape_and_normalize(images):
-    ### START CODE HERE
+    # Define input and output tensors with the values for houses with 1 up to 6 bedrooms
+    # Hint: Remember to explicitly set the dtype as float
+    xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], dtype=float)
+    ys = np.array([1.0, 1.5, 2.0, 2.5, 3.0, 3.5], dtype=float) * 100  # multiplying by 100 to scale the values
 
-    # Reshape the images to add an extra dimension
-    images_normalized = images.reshape(-1, 28, 28, 1)
-
-    # Normalize pixel values
-    images_normalized = images_normalized / 255.0
-    ### END CODE HERE
-
-    return images_normalized
-
-# Reload the images in case you run this cell multiple times
-(training_images, _), _ = tf.keras.datasets.mnist.load_data()
-
-# Apply your function
-training_images = reshape_and_normalize(training_images)
-
-print(f"Maximum pixel value after normalization: {np.max(training_images)}\n")
-print(f"Shape of training set after reshaping: {training_images.shape}\n")
-print(f"Shape of one image after reshaping: {training_images[0].shape}")
-
-# GRADED CLASS: myCallback
-### START CODE HERE
-
-# Remember to inherit from the correct class
-class myCallback(tf.keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs={}):
-        if logs.get('accuracy') > 0.995:
-            print("\nReached 99.5% accuracy so cancelling training!")
-            self.model.stop_training = True
-### END CODE HERE
-
-# GRADED FUNCTION: convolutional_model
-def convolutional_model():
-    ### START CODE HERE
-
-    # Define the model
+    # Define your model (should be a model with 1 dense layer and 1 unit)
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-        tf.keras.layers.MaxPooling2D((2, 2)),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(10, activation='softmax')
+        tf.keras.layers.Dense(units=1, input_shape=[1])
     ])
-    ### END CODE HERE
 
-    # Compile the model
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
+    # Compile your model
+    # Set the optimizer to Stochastic Gradient Descent
+    # and use Mean Squared Error as the loss function
+    model.compile(optimizer='sgd', loss='mean_squared_error')
 
+    # Train your model for 1000 epochs by feeding the i/o tensors
+    model.fit(xs, ys, epochs=1000)
+
+    ### END CODE HERE ###
     return model
 
-# Save your untrained model
-model = convolutional_model()
 
-# Instantiate the callback class
-callbacks = myCallback()
-
-# Train your model (this can take up to 5 minutes)
-history = model.fit(training_images, training_labels, epochs=10, callbacks=[callbacks])
-
-print(f"Your model was trained for {len(history.epoch)} epochs")
+# Get your trained model
+model = house_model()
+new_y = np.array([7.0])
+prediction = model.predict([new_y])[0]
+print(prediction)
